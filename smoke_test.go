347f0763147ab53d4464b246e22b8373655f5de1
rhysd <lin90162@yahoo.co.jp>
2020-02-18 13:53:34 +0900

    use regex to check stdout

--- smoke_test.go
+++ smoke_test.go
@@ -5,11 +5,30 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"testing"
 )
 
 func TestSmoke(t *testing.T) {
+	exe := "changelog-from-release"
+	if runtime.GOOS == "windows" {
+		exe = exe + ".exe"
+	}
+
+	if _, err := os.Stat(exe); err != nil {
+		t.Fatal("Executable not found:", exe)
+	}
+
+	if s, err := os.Stat(".git"); err != nil || !s.IsDir() {
+		t.Fatal("Test did not run at root of repository")
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
 	for _, tc := range []struct {
 		args   []string
 		stdout string
@@ -20,40 +39,24 @@ func TestSmoke(t *testing.T) {
 		},
 		{
 			[]string{"-t"},
-			version + "\n",
+			`v\d+\.\d+\.\d+\n$`,
 		},
 		{
 			[]string{"-v"},
-			version + "\n",
+			`v\d+\.\d+\.\d+\n$`,
 		},
 	} {
 		t.Run(fmt.Sprint(tc.args), func(t *testing.T) {
-			exe := "changelog-from-release"
-			if runtime.GOOS == "windows" {
-				exe = exe + ".exe"
-			}
-
-			if _, err := os.Stat(exe); err != nil {
-				t.Fatal("Executable not found:", exe)
-			}
-
-			if s, err := os.Stat(".git"); err != nil || !s.IsDir() {
-				t.Fatal("Test did not run at root of repository")
-			}
-
-			cwd, err := os.Getwd()
-			if err != nil {
-				panic(err)
-			}
-
 			p := filepath.Join(cwd, exe)
 			b, err := exec.Command(p, tc.args...).CombinedOutput()
 			out := string(b)
 			if err != nil {
 				t.Fatal(err, out, p)
 			}
-			if out != tc.stdout {
-				t.Fatalf("Should output %#v but got %#v", tc.stdout, out)
+
+			re := regexp.MustCompile(tc.stdout)
+			if !re.MatchString(out) {
+				t.Fatalf("Output %#v did not match to %#v", out, tc.stdout)
 			}
 
 			git, err := NewGitForCwd()
