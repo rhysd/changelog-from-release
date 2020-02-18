8ea32a57763aea633f2ef18ebe340912d6db23dc
rhysd <lin90162@yahoo.co.jp>
2020-02-18 12:50:16 +0900

    add more tests for command line options

--- smoke_test.go
+++ smoke_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -9,40 +10,60 @@ import (
 )
 
 func TestSmoke(t *testing.T) {
-	exe := "changelog-from-release"
-	if runtime.GOOS == "windows" {
-		exe = exe + ".exe"
-	}
+	for _, tc := range []struct {
+		args   []string
+		stdout string
+	}{
+		{
+			[]string{},
+			"",
+		},
+		{
+			[]string{"-t"},
+			version + "\n",
+		},
+		{
+			[]string{"-v"},
+			version + "\n",
+		},
+	} {
+		t.Run(fmt.Sprint(tc.args), func(t *testing.T) {
+			exe := "changelog-from-release"
+			if runtime.GOOS == "windows" {
+				exe = exe + ".exe"
+			}
 
-	if _, err := os.Stat(exe); err != nil {
-		t.Fatal("Executable not found:", exe)
-	}
+			if _, err := os.Stat(exe); err != nil {
+				t.Fatal("Executable not found:", exe)
+			}
 
-	if s, err := os.Stat(".git"); err != nil || !s.IsDir() {
-		t.Fatal("Test did not run at root of repository")
-	}
+			if s, err := os.Stat(".git"); err != nil || !s.IsDir() {
+				t.Fatal("Test did not run at root of repository")
+			}
 
-	cwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
+			cwd, err := os.Getwd()
+			if err != nil {
+				panic(err)
+			}
 
-	p := filepath.Join(cwd, exe)
-	b, err := exec.Command(p).CombinedOutput()
-	out := string(b)
-	if err != nil {
-		t.Fatal(err, out, p)
-	}
-	if out != "" {
-		t.Fatalf("Should output nothing %#v", out)
-	}
+			p := filepath.Join(cwd, exe)
+			b, err := exec.Command(p, tc.args...).CombinedOutput()
+			out := string(b)
+			if err != nil {
+				t.Fatal(err, out, p)
+			}
+			if out != tc.stdout {
+				t.Fatalf("Should output %#v but got %#v", tc.stdout, out)
+			}
 
-	git, err := NewGitForCwd()
-	if err != nil {
-		t.Fatal(err)
-	}
+			git, err := NewGitForCwd()
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if out, err = git.Exec("diff", "--quiet"); err != nil {
-		t.Fatal("Repository should not be dirty:", out, err)
+			if out, err = git.Exec("diff", "--quiet"); err != nil {
+				t.Fatal("Repository should not be dirty:", out, err)
+			}
+		})
 	}
 }
