9f5486b669b9845a2a2fb1218e12b767a806c905
rhysd <lin90162@yahoo.co.jp>
2020-02-19 16:31:39 +0900

    ensure to fetch all tags in action

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 cd "$GITHUB_WORKSPACE" || exit 1
 
 if [ -z "$INPUT_VERSION" ]; then
-    INPUT_VERSION="$(git describe --tags)"
+    git fetch --depth=1 origin '+refs/tags/*:refs/tags/*'
+    INPUT_VERSION="$(git tag --list | tail -n 1)"
 fi
 
 echo "changelog-from-release version: $(/changelog-from-release -v)"
@@ -14,8 +15,8 @@ echo "Running changelog-from-release to update ${INPUT_FILE} ..."
 GITHUB_TOKEN="$INPUT_GITHUB_TOKEN" /changelog-from-release > "${INPUT_FILE}"
 
 echo "Committing changes in ${INPUT_FILE} ..."
-git add "${INPUT_FILE}"
-git commit -m "Update changelog for ${INPUT_VERSION}"
+git -c "user.name=${GITHUB_ACTOR}" -c "user.email=${GITHUB_ACTOR}@users.noreply.github.com" add "${INPUT_FILE}"
+git -c "user.name=${GITHUB_ACTOR}" -c "user.email=${GITHUB_ACTOR}@users.noreply.github.com" commit -m "Update changelog for ${INPUT_VERSION}"
 
 if [ "$INPUT_PUSH" = 'true' ]; then
     echo 'Pushing generated commit to remote'
