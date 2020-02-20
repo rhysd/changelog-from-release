db067205ff509330f43912f6ea62209a411546c5
rhysd <lin90162@yahoo.co.jp>
2020-02-20 10:45:58 +0900

    fix pushing changes to master

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -5,9 +5,12 @@ set -e
 cd "$GITHUB_WORKSPACE" || exit 1
 
 if [ -z "$INPUT_VERSION" ]; then
-    git fetch --depth=1 origin '+refs/tags/*:refs/tags/*'
+    git fetch --prune --depth=1 origin '+refs/tags/*:refs/tags/*'
     INPUT_VERSION="$(git tag --list | tail -n 1)"
 fi
+
+echo "::debug::Retrieved version: ${INPUT_VERSION}"
+echo "::debug::Push to remote?: ${INPUT_PUSH}"
 
 echo "changelog-from-release version: $(/changelog-from-release -v)"
 
@@ -15,13 +18,19 @@ echo "Running changelog-from-release to update ${INPUT_FILE} ..."
 GITHUB_TOKEN="$INPUT_GITHUB_TOKEN" /changelog-from-release > "${INPUT_FILE}"
 
 echo "Committing changes in ${INPUT_FILE} ..."
-git -c "user.name=${GITHUB_ACTOR}" -c "user.email=${GITHUB_ACTOR}@users.noreply.github.com" add "${INPUT_FILE}"
-git -c "user.name=${GITHUB_ACTOR}" -c "user.email=${GITHUB_ACTOR}@users.noreply.github.com" commit -m "Update changelog for ${INPUT_VERSION}"
+
+set -x
+git add "${INPUT_FILE}"
+git \
+    -c "user.name=${GITHUB_ACTOR}" \
+    -c "user.email=${GITHUB_ACTOR}@users.noreply.github.com" \
+    commit -m "Update changelog for ${INPUT_VERSION}
+
+This commit was created by changelog-from-release in '${GITHUB_WORKFLOW}' CI workflow"
 
 if [ "$INPUT_PUSH" = 'true' ]; then
-    echo 'Pushing generated commit to remote'
-    MY_REPO_URL="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
-    git push --force-with-lease "${MY_REPO_URL}"
+    git push --force "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 fi
+set +x
 
 echo 'Done'
