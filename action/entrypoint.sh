299577b156f718e833dfe5fbab873baa061c082a
rhysd <lin90162@yahoo.co.jp>
2022-08-04 14:05:38 +0900

    add `commit-summary-template` input to action

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -9,7 +9,7 @@ git config --global --add safe.directory /github/workspace
 cd "$GITHUB_WORKSPACE" || exit 1
 
 if [ -z "$INPUT_VERSION" ]; then
-    TAG_FROM_PAYLOAD="$(jq .release.tag_name < ${GITHUB_EVENT_PATH})"
+    TAG_FROM_PAYLOAD="$(jq .release.tag_name < "$GITHUB_EVENT_PATH")"
     if [ "$TAG_FROM_PAYLOAD" = "null" ]; then
         git fetch --prune --depth=1 origin '+refs/tags/*:refs/tags/*'
         INPUT_VERSION="$(git tag --list | tail -n 1)"
@@ -23,6 +23,7 @@ fi
 echo "::debug::Retrieved version: ${INPUT_VERSION}"
 echo "::debug::Make a commit?: ${INPUT_COMMIT}"
 echo "::debug::Push to remote?: ${INPUT_PUSH}"
+echo "::debug::Commit summary template: '${INPUT_COMMIT_SUMMARY_TEMPLATE}'"
 
 echo "changelog-from-release version: $(/changelog-from-release -v)"
 
@@ -31,12 +32,17 @@ GITHUB_TOKEN="$INPUT_GITHUB_TOKEN" /changelog-from-release > "${INPUT_FILE}"
 set +x
 
 if [ "$INPUT_COMMIT" = 'true' ]; then
+    COMMIT_SUMMARY="Update changelog for ${INPUT_VERSION}"
+    if [ -n "$INPUT_COMMIT_SUMMARY_TEMPLATE" ]; then
+        COMMIT_SUMMARY="$(printf "$INPUT_COMMIT_SUMMARY_TEMPLATE" "$INPUT_VERSION")"
+    fi
+
     set -x
     git add "${INPUT_FILE}"
     git \
         -c "user.name=${GITHUB_ACTOR}" \
         -c "user.email=${GITHUB_ACTOR}@users.noreply.github.com" \
-        commit -m "Update changelog for ${INPUT_VERSION}
+        commit -m "${COMMIT_SUMMARY}
 
     This commit was created by changelog-from-release in '${GITHUB_WORKFLOW}' CI workflow"
 
